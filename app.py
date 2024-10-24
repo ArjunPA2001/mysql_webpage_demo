20a7e6634789a60d017a1fc103f6027334137ca4
Arjun <arjunpa2001@gmail.com>
2024-10-24 09:29:37 +0530

    added insert and delete

--- app.py
+++ app.py
@@ -1,26 +1,102 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g, url_for, redirect, request
 import mysql.connector
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    try:
-        mydb = mysql.connector.connect(
+def get_db():
+    if 'db' not in g:
+        g.db = mysql.connector.connect(
         host="localhost",
         user="root",
         password="pass",
         port = 3333
         )
+        return g.db
+    
+    else:
+        return g.db
 
-        curr = mydb.cursor()
+def execute_query(query, q_type = 'select'):
+    try:
+        mydb = get_db()
+        with mydb.cursor() as curr:
+            curr.execute(query)
+
+            if q_type == "CRUD":
+                mydb.commit()
+            resp = curr.fetchall()
+
+        return resp
 
     except Exception as e:
-        print(e)
-        return str(e)
-    curr.execute("select * from `sys`.`customer_table`;")
-    resp = curr.fetchall()
-    return render_template('index.html',customers = resp)
+        print("an exception occured:")
+        s = f"%s: %s" % (type(e).__name__,e)
+        print(s)
+        return s
 
-def display():
-    pass
+@app.route("/")
+def home():
+    query = "select * from `sys`.`customer_table`;"
+    resp = execute_query(query)
+
+    if isinstance(resp, str):
+        return resp
+    else:
+        return render_template('index.html',customers = resp)
+
+@app.route("/insert")
+def insert():
+    query = "select * from `sys`.`customer_table`;"
+    resp = execute_query(query)
+
+    if isinstance(resp, str):
+        return resp
+    else:
+        return render_template('insert.html',customers = resp)
+
+@app.route("/delete")
+def delete():
+    query = "select * from `sys`.`customer_table`;"
+    resp = execute_query(query)
+
+    if isinstance(resp, str):
+        return resp
+    else:
+        return render_template('delete.html',customers = resp)
+
+@app.route("/insert_form",methods=["POST"])
+def insert_form():
+    customer_id = request.form['customer_id']
+    customer_name = request.form['customer_name']
+    customer_type = request.form['customer_type']
+    date_time = request.form['date_time']
+
+    query = f"insert into `sys`.`customer_table` values (%d,'%s','%s','%s:00');" % (int(customer_id),customer_name,customer_type,' '.join(date_time.split('T')))
+    resp = execute_query(query,q_type="CRUD")
+
+    if isinstance(resp, str):
+        return resp
+    else:
+        return redirect('/')
+    
+@app.route("/delete_form",methods=["POST"])
+def delete_form():
+    filters = []
+
+    for k,v in request.form.items():
+        if v == '':
+            continue
+            
+        if k == 'customer_id':
+            filters.append(k+" = "+v)
+        else:
+            filters.append(k+" = '"+v+"'")
+
+    query = f"delete from `sys`.`customer_table` where %s;" % (' and '.join(filters))
+    print(query)
+    resp = execute_query(query,q_type="CRUD")
+
+    if isinstance(resp, str):
+        return resp
+    else:
+        return redirect('/')
